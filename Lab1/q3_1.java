002241a498b3e69da563422c2663d86f872a314e
cmanage1 <cmanage1@users.noreply.github.com>
2020-01-15 17:45:08 -0500

    Initial commit

---
+++ Lab1/q3_1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Q3_SquareArray{
+
+	public static int[] createArray(int size) {
+        int[] arr= new arr[size];
+
+        for (int i=0; i<= size; i++){
+            arr[i] = i*i;
+        }
+        return arr;
+	}
+
+	public static void main(String[] args){
+		for (int j=0; j<=size; i++){
+            System.out.println(arr[j]);
+        }
+	}
+}
