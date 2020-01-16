a5b6272d28ef2e7fe3dae7b73893dee31cbef7e3
cmanage1 <cmanage1@users.noreply.github.com>
2020-01-16 15:03:18 -0500

    works fine

---
+++ Lab1/q3_4.java
@@ -0,0 +1,25 @@
+public class Q3_ReverseSortDemo {
+
+	public static void main(String[] args){
+		char[] unorderedLetters;
+		unorderedLetters = new char[]{'b', 'm', 'z', 'a', 'u'};
+		reverseSort(unorderedLetters);
+		for (int i = 0 ; i < unorderedLetters.length; i++ )
+			System.out.print(unorderedLetters[i]);
+	}
+
+	//method that sorts a char array into its reverse alphabetical order
+	public static void reverseSort(char[] values){
+        char temp = 't';
+        //Swapping first element with last element
+        //for half the array...
+        //If there is uneven letters, middle gets ignored
+        //which is fine...
+		for(int i=0; i < values.length/2 ; i++){
+            temp = values[i];
+            values[i] = values[values.length -1 -i];
+            values[values.length-1-i] = temp;
+        }
+	}
+
+}
