f5f5cb9247ff81f6879f2a33c2af5cc549101028
cmanage1 <55030581+cmanage1@users.noreply.github.com>
2020-01-15 21:14:17 -0500

    Update q3_3.java

--- Lab1/q3_3.java
+++ Lab1/q3_3.java
@@ -4,9 +4,31 @@ public class Q3_ArrayInsertionDemo{
 
 	public static int[] insertIntoArray(int[] beforeArray, int indexToInsert, int valueToInsert){
 		// Your code here
+		int arr[]= new int[11];
+		int i=0;
+		while (i< arr.length){
+			if (i==indexToInsert){
+				arr[i] = valueToInsert;
+				i= i+2;
+			}
+			else i++;
+		return arr;
 	}
 
 	public static void main(String[] args){
 		// Your code here
+		int arr1[]= {1,2,3,4,5,6,7,8,9,10};
+		System.out.println("Array before insertion:);
+		for (int i=0; i<arr1.length ; i++){
+			System.out.println(arr1[i]);
+		}
+		int index= 4;
+		int value= 56;
+		int arr2[] = insertIntoArray(arr1,index,value);
+		System.out.println("Array after insertion of " +Integer.toString(value)
+				   +" position " + Integer.toString(index) )
+		for (int i=0; i<arr2.length; i++){
+			System.out.println(arr2[i]);
+		}	
 	}
 }
