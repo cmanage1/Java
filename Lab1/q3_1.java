ea9fafa4da3394c63016d443a8db24ba307d32ca
cmanage1 <cmanage1@users.noreply.github.com>
2020-01-15 19:52:57 -0500

    Update q3_1.java

--- Lab1/q3_1.java
+++ Lab1/q3_1.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Q3_SquareArray{
 
 	public static int[] createArray(int size) {
-        int[] arr= new arr[size];
+        int arr[]= new int[size+1];
 
         for (int i=0; i<= size; i++){
             arr[i] = i*i;
@@ -14,8 +14,12 @@ public class Q3_SquareArray{
 	}
 
 	public static void main(String[] args){
-		for (int j=0; j<=size; i++){
-            System.out.println(arr[j]);
-        }
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		int arr[]= createArray(num);
+
+		for (int j=0; j<= num; j++){
+			System.out.println("The square of "+  Integer.toString(j)+ " is: " + Integer.toString(arr[j]));
+		}
 	}
 }
