92de8ae10373af5dc83c903b5242180f1a71d3fa
cmanage1 <chethinlm@gmail.com>
2020-02-22 19:25:14 -0500

    working q2

--- a2_300066367_300081135/Q2/ListOfGamesGenerator.java
+++ a2_300066367_300081135/Q2/ListOfGamesGenerator.java
@@ -1,11 +1,11 @@
 import java.util.LinkedList;
 
 public class ListOfGamesGenerator {
-	
+
 
    /**
 	* generates all different games for the specified
-	* parameters. Each game is recorded only once. 
+	* parameters. Each game is recorded only once.
 	* once a game is finished, it is not extended further
 	* @param lines
     *  the number of lines in the game
@@ -18,7 +18,28 @@ public class ListOfGamesGenerator {
   	*/
 	public static LinkedList<LinkedList<TicTacToeGame>> generateAllGames(int lines, int columns, int winLength){
 
-		//YOUR CODE HERE
+		// 	2d array
+		LinkedList<TicTacToeGame> games= new LinkedList<TicTacToeGame>();
 
+		LinkedList<LinkedList<TicTacToeGame>> res= new LinkedList<LinkedList<TicTacToeGame>>();
+
+
+		for (int i=0; i< lines*columns; i++){
+			TicTacToeGame baseGame = new TicTacToeGame(lines, columns, winLength);
+
+			for (int j=0; j< (lines*columns); j++){
+				TicTacToeGame currentGame = new TicTacToeGame(baseGame, j);
+				currentGame.play(j);
+
+				if (currentGame.getGameState() != GameState.PLAYING){
+					games.add(currentGame);;
+					break;
+				}
+
+			}
+			res.add(games);
+		}
+
+		return res;
 	}
 }
