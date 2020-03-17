05311ee678848ccf631132e3c016b62c130f316e
cmanage1 <chethinlm@gmail.com>
2020-03-17 12:24:37 -0400

    lab8 finished

--- Lab8/Cashier.java
+++ Lab8/Cashier.java
@@ -1,3 +1,59 @@
 public class Cashier{
-    
+
+    private static Queue<Customer> queue;
+    private static Customer currentCustomer;
+    private int totalCustomerWaitTime;
+    private int customersServed;
+    private int totalItemsServed;
+
+    public Cashier(){
+        this.queue = new ArrayQueue<Customer>();
+
+        this.totalCustomerWaitTime=0;
+        this.customersServed = 0;
+        this.totalItemsServed= 0;
+    }
+
+    public void addCustomer(Customer c){
+        queue.enqueue(c);
+    }
+
+    public int getQueueSize(){
+        return queue.size();
+    }
+
+    public void serveCustomers(int currentTime){
+
+        if (currentCustomer == null && queue.isEmpty()) {
+            return;
+
+        }
+
+        if (currentCustomer == null){
+            currentCustomer = queue.dequeue();
+            totalCustomerWaitTime += (currentTime - currentCustomer.getArrivalTime());
+            customersServed++;
+        }
+
+        currentCustomer.serve();
+
+        if (currentCustomer.getNumberOfItems() ==0 ){
+            totalItemsServed+= currentCustomer.getNumberOfServedItems();
+            currentCustomer=null; //scrubbing the queue
+        }
+
+    }
+
+    public int getTotalCustomerWaitTime(){
+        return totalCustomerWaitTime;
+    }
+
+    public int getTotalCustomersServed(){
+        return customersServed;
+    }
+
+    public int getTotalItemsServed(){
+        return totalItemsServed;
+    }
+
 }
--- Lab8/Customer.java
+++ Lab8/Customer.java
@@ -1,3 +1,35 @@
+import java.util.Random;
+
 public class Customer{
-    
+
+    private int arrivalTime;
+    private int initialNumberOfItems;
+    private int numberOfItems;
+    private final int MAX_NUM_ITEMS =25;
+
+    public Customer(int time){
+        Random generator;
+        generator = new Random();
+
+        this.numberOfItems = generator.nextInt(MAX_NUM_ITEMS-1)+1;
+
+        this.arrivalTime = time;
+        this.initialNumberOfItems=numberOfItems;
+    }
+
+    public int getArrivalTime(){
+        return arrivalTime;
+    }
+
+    public int getNumberOfItems(){
+        return numberOfItems;
+    }
+
+    public int getNumberOfServedItems(){
+        return initialNumberOfItems - numberOfItems;
+    }
+
+    public void serve(){
+        numberOfItems--;
+    }
 }
