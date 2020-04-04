361dc5720b81ca748708b732f71f6d6081e320f7
cmanage1 <chethinlm@gmail.com>
2020-04-04 19:33:47 -0400

    TicTacToeGame error fixed

--- a4_300066367_300081135/ComputerMenacePlayer.java
+++ a4_300066367_300081135/ComputerMenacePlayer.java
@@ -4,5 +4,8 @@ import java.util.LinkedList;
 public class ComputerMenacePlayer extends Player {
 
 	// YOUR CODE HERE
+	public ComputerMenacePlayer(){
+
+	}
 
 }
--- a4_300066367_300081135/TicTacToeGame.java
+++ a4_300066367_300081135/TicTacToeGame.java
@@ -69,7 +69,6 @@ public class TicTacToeGame {
 
 	private int currentTransformation;
 
-	private int[] transformedBoard;
 
 
 
