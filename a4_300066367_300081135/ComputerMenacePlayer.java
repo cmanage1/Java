f3b0f1edf4d9bf5f41f30cbf43ddb871a367d611
cmanage1 <chethinlm@gmail.com>
2020-04-05 13:47:00 -0400

    upodated 2 menace classes

--- a4_300066367_300081135/ComputerMenacePlayer.java
+++ a4_300066367_300081135/ComputerMenacePlayer.java
@@ -3,9 +3,27 @@ import java.util.LinkedList;
 
 public class ComputerMenacePlayer extends Player {
 
-	// YOUR CODE HERE
+	//in contructor make the beads
+	//in play manipulate the beads and 
+	private LinkedList<LinkedList<MenaceTicTacToeGame>>> allList; 
+
 	public ComputerMenacePlayer(){
+		super()
+
+		allGames = new LinkedList<LinkedList<>();
+
 
 	}
+
+	public void play(TicTacToeGame game){
+		
+		if (game.getLevel() == game.lines * game.columns) {
+			throw new IllegalArgumentException("Game is finished already!");
+		}
+
+
+	}
+
+
 
 }
